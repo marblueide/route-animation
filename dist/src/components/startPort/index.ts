39d5de9155630fb858bfbb750eea5ac36d3ad99a
K <66667134+marblueide@users.noreply.github.com>
2022-12-05 19:15:51 +0800

    undo pages

--- dist/src/components/startPort/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./StarportCraft"
-export * from "./StartPortProxy"
-export * from "./StartProxy"
-export * from "./StarportCrarrier"
-
-
-
-
-
--- dist/src/router/index.ts
+++
@@ -1,7 +0,0 @@
-import {createRouter,RouteRecordRaw,createWebHistory} from "vue-router"
-import routes from '~pages'
-
-export default createRouter({
-  routes,
-  history: createWebHistory()
-})
